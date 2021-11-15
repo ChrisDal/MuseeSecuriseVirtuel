f032bf5ea57a0399e454d81339b6db7f1c8070ac
ChrisDal <cesaria77550@gmail.com>
2021-11-15 09:11:38 +0100

    Refactoring Functions

---
+++ code/imageanalysis.h
@@ -0,0 +1,117 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+
+// ===================================================
+// HISTOGRAMS 
+/*const int histSize = 256;
+float range[] = { 0, 256 }; //the upper boundary is exclusive
+const float* histRange[] = { range };
+
+bool uniform = true; 
+bool accumulate = false;
+
+cv::Mat grey_hist, grey_hist_perm, grey_hist_rec;
+
+cv::calcHist( &image, 1, 0, cv::Mat(), grey_hist, 1, &histSize, histRange, uniform, accumulate );
+cv::calcHist( &permutedImage, 1, 0, cv::Mat(), grey_hist_perm, 1, &histSize, histRange, uniform, accumulate );
+cv::calcHist( &reconstructedImg, 1, 0, cv::Mat(), grey_hist_rec, 1, &histSize, histRange, uniform, accumulate );
+
+// normalized histogram
+int hist_w = 512;
+int hist_h = 400;
+int bin_w = cvRound( (double) hist_w/histSize );
+
+cv::Mat histImage( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0) );
+
+cv::normalize(grey_hist, grey_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
+cv::normalize(grey_hist_perm, grey_hist_perm, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
+cv::normalize(grey_hist_rec, grey_hist_rec, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
+
+for( int i = 1; i < histSize; i++ )
+{
+
+    cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(grey_hist.at<float>(i-1)) ),
+            cv::Point( bin_w*(i), hist_h - cvRound(grey_hist.at<float>(i)) ),
+            cv::Scalar( 255, 0, 0),
+            2, 8, 0  );
+
+    cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(grey_hist_perm.at<float>(i-1)) ),
+            cv::Point( bin_w*(i), hist_h - cvRound(grey_hist_perm.at<float>(i)) ),
+            cv::Scalar( 0, 255, 0),
+            2, 8, 0  );
+
+    cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(grey_hist_rec.at<float>(i-1)) ),
+            cv::Point( bin_w*(i), hist_h - cvRound(grey_hist_rec.at<float>(i)) ),
+            cv::Scalar( 0, 0, 255), 
+            2, 8, 0  );
+}*/
+
+
+
+/*===========================================================================*/
+// HISTOGRAM ON GRAYSCALE 
+
+// Input : Mat Image , Histogram Color 
+cv::Mat processBarHistogram(cv::Mat& image, cv::Scalar color = cv::Scalar( 255, 0, 0))
+{
+    // HISTOGRAMS 
+    const int histSize = 256;
+    float range[] = { 0, 256 }; //the upper boundary is exclusive
+    const float* histRange[] = { range };
+
+    static bool uniform = true; 
+    static bool accumulate = false;
+
+    cv::Mat histo;
+    // calculate histogram
+    cv::calcHist( &image, 1, 0, cv::Mat(), histo, 1, &histSize, histRange, uniform, accumulate );
+
+    // normalized histogram
+    int hist_w = 512;
+    int hist_h = 400;
+    int bin_w = cvRound( (double) hist_w/histSize );
+    cv::Mat histoImg( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0) );
+    cv::normalize(histo, histo, 0, histoImg.rows, cv::NORM_MINMAX, -1, cv::Mat() );
+
+    for( int i = 0; i < histSize; i++ )
+    {
+        float y = hist_h - cvRound(histo.at<float>(i)) ; 
+
+        cv::rectangle( histoImg, 
+                cv::Point2f(bin_w*i - float(bin_w)/2.0f, hist_h),
+                cv::Point2f(bin_w*i + float(bin_w)/2.0f, y),
+                color,
+                cv::FILLED, 8, 0  
+                );
+    }
+
+    return histoImg;
+}
+
+
+/*===========================================================================*/
+// DISPLAY Pack of images 
+// same size as A required for all images 
+void concatenateFourMat(cv::Mat& concatdisplayImage, const cv::Mat& A, const cv::Mat& B , const cv::Mat& C,const cv::Mat& D)
+{
+   // display our images 
+    concatdisplayImage = cv::Mat(cv::Size(A.cols * 2, A.rows * 2), A.type(), cv::Scalar::all(0)); 
+    // Create ROI and Copy A to it
+    cv::Mat matRoi = concatdisplayImage(cv::Rect(0,0,A.cols, A.rows));
+    A.copyTo(matRoi);
+    // Copy B to ROI
+    matRoi = concatdisplayImage(cv::Rect(A.cols, 0, A.cols, A.rows));
+    B.copyTo(matRoi);
+    // Copy C to ROI 
+    matRoi = concatdisplayImage(cv::Rect(0,A.rows,A.cols,A.rows));
+    C.copyTo(matRoi);
+    // Copy D to ROI 
+    matRoi = concatdisplayImage(cv::Rect(A.cols,A.rows,A.cols,A.rows));
+    D.copyTo(matRoi);
+
+}
+
+
+
+
+
--- code/naive.cpp
+++ code/naive.cpp
@@ -3,6 +3,7 @@
 #include <vector> 
 #include <numeric> 
 #include "BasePermutation.h"
+#include "imageanalysis.h"
 
 #include <opencv2/core/types.hpp>
 #include <opencv2/core/fast_math.hpp>
@@ -12,45 +13,6 @@
 #include <opencv2/imgcodecs.hpp>
 
 #define IMAGEMAT_TYPE uchar 
-
-/*===========================================================================*/
-// histogram on greyscale
-cv::Mat processBarHistogram(cv::Mat& image, cv::Scalar color = cv::Scalar( 255, 0, 0))
-{
-    // HISTOGRAMS 
-    const int histSize = 256;
-    float range[] = { 0, 256 }; //the upper boundary is exclusive
-    const float* histRange[] = { range };
-
-    static bool uniform = true; 
-    static bool accumulate = false;
-
-    cv::Mat histo;
-    // calculate histogram
-    cv::calcHist( &image, 1, 0, cv::Mat(), histo, 1, &histSize, histRange, uniform, accumulate );
-
-    // normalized histogram
-    int hist_w = 512;
-    int hist_h = 400;
-    int bin_w = cvRound( (double) hist_w/histSize );
-    cv::Mat histoImg( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0) );
-    cv::normalize(histo, histo, 0, histoImg.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-
-    for( int i = 0; i < histSize; i++ )
-    {
-        float y = hist_h - cvRound(histo.at<float>(i)) ; 
-
-        cv::rectangle( histoImg, 
-                cv::Point2f(bin_w*i - float(bin_w)/2.0f, hist_h),
-                cv::Point2f(bin_w*i + float(bin_w)/2.0f, y),
-                color,
-                cv::FILLED, 8, 0  
-                );
-    }
-
-    return histoImg;
-}
-
 
 
 /*===========================================================================*/
@@ -111,70 +73,25 @@ int main(int argc, char** argv )
 
     // ===================================================
     // HISTOGRAMS 
-    /*const int histSize = 256;
-    float range[] = { 0, 256 }; //the upper boundary is exclusive
-    const float* histRange[] = { range };
-
-    bool uniform = true; 
-    bool accumulate = false;
-
-    cv::Mat grey_hist, grey_hist_perm, grey_hist_rec;
-
-    cv::calcHist( &image, 1, 0, cv::Mat(), grey_hist, 1, &histSize, histRange, uniform, accumulate );
-    cv::calcHist( &permutedImage, 1, 0, cv::Mat(), grey_hist_perm, 1, &histSize, histRange, uniform, accumulate );
-    cv::calcHist( &reconstructedImg, 1, 0, cv::Mat(), grey_hist_rec, 1, &histSize, histRange, uniform, accumulate );
-
-    // normalized histogram
-    int hist_w = 512;
-    int hist_h = 400;
-    int bin_w = cvRound( (double) hist_w/histSize );
-
-    cv::Mat histImage( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0) );
-
-    cv::normalize(grey_hist, grey_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-    cv::normalize(grey_hist_perm, grey_hist_perm, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-    cv::normalize(grey_hist_rec, grey_hist_rec, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-
-    for( int i = 1; i < histSize; i++ )
-    {
-
-        cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(grey_hist.at<float>(i-1)) ),
-              cv::Point( bin_w*(i), hist_h - cvRound(grey_hist.at<float>(i)) ),
-              cv::Scalar( 255, 0, 0),
-              2, 8, 0  );
-
-        cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(grey_hist_perm.at<float>(i-1)) ),
-              cv::Point( bin_w*(i), hist_h - cvRound(grey_hist_perm.at<float>(i)) ),
-              cv::Scalar( 0, 255, 0),
-              2, 8, 0  );
-
-        cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(grey_hist_rec.at<float>(i-1)) ),
-              cv::Point( bin_w*(i), hist_h - cvRound(grey_hist_rec.at<float>(i)) ),
-              cv::Scalar( 0, 0, 255), 
-              2, 8, 0  );
-    }*/
-
-
     cv::Mat histoBarHimg = processBarHistogram(image, cv::Scalar( 255, 0, 0)); 
     cv::Mat histoBarHperm = processBarHistogram(permutedImage, cv::Scalar( 0, 255, 0)); 
     cv::Mat histoBarHrec = processBarHistogram(reconstructedImg, cv::Scalar( 0, 0, 255)); 
 
     // ===================================================
+    // DISPLAY 
+    // =======
+    
+    // display our images
+    cv::Mat displayImages;  
+    concatenateFourMat(displayImages, image, cv::Mat(), permutedImage, reconstructedImg); 
+
+    // display our histograms  
+    cv::Mat displayHistos;  
+    concatenateFourMat(displayHistos, histoBarHimg, cv::Mat(), histoBarHperm, histoBarHrec); 
 
 
-
-    // display our images 
-    /*cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
-    cv::namedWindow("Display Permuted Image", cv::WINDOW_AUTOSIZE );
-    cv::namedWindow("Display Reconstructed Image", cv::WINDOW_AUTOSIZE );*/
-
-    cv::imshow("Display Image", image);
-    cv::imshow("Display Permuted image", permutedImage);
-    cv::imshow("Display Reconstructed image", reconstructedImg);
-
-    cv::imshow("CalcHist barH Image ", histoBarHimg );
-    cv::imshow("CalcHist barH  Permuted Image", histoBarHperm );
-    cv::imshow("CalcHist barH Reconstructed image", histoBarHrec );
+    cv::imshow("Display Histogram: A Original, C Permuted Image, D Reconstructed ", displayHistos );
+    cv::imshow("images: A Original, C Permuted Image, D Reconstructed", displayImages );
 
     // End 
     cv::waitKey(0);
